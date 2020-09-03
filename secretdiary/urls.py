e32c00474ae13f559fc1ef1a0a1fbefc7fdc5beb
s0y00n <leeesoyoon@gmail.com>
2020-09-03 16:27:02 +0900

    board&diarypage html+css

--- secretdiary/urls.py
+++ secretdiary/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from diary import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('board', views.board, name="borad.html"),
+    path('diarypage', views.diarypage, name="diarypage.html")
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
