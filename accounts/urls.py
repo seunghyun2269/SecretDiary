7a7e96f6f32bbdd1fb67b5d368f20a476af96ee0
wille318@naver.com <wille318@naver.com>
2020-09-03 17:57:36 +0900

    main page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('login', views.login_view, name= "login" ),
+    # path('login', views.login_view, name= "login" ),
     path('logout', views.logout_view, name= "logout"),
     path('register', views.register_view, name='register'), 
 ]
--- secretdiary/urls.py
+++ secretdiary/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from diary import views
+from accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('board', views.board, name="borad.html"),
-    path('diarypage', views.diarypage, name="diarypage.html"),
+    path('', views.login_view, name= "login" ),
     path('auth/', include("accounts.urls")), 
     path('diary/', include("diary.urls")), 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
