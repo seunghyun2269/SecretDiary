ba4245015e01ed533e742d15374e7e4f06b7f6c7
seunghyun2269 <akdntm2269@naver.com>
2020-09-09 02:55:38 +0900

    edit

--- diary/models.py
+++ diary/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-# 글(post)모델: 생성 날짜, 작성자, 글 내용, 글 id, 제목
 class Post(models.Model):
     # author = models.ForeignKey(settings.AUTH_USER_MODEL) # User모델의 userid 참조
     title = models.CharField(max_length=100)
