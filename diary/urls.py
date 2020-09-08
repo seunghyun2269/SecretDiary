29c18c164437035c69b0fadcdfcce7e6fe29681a
seunghyun2269 <akdntm2269@naver.com>
2020-09-09 01:52:42 +0900

    edit urls.py

--- diary/urls.py
+++ diary/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name = 'main'),
     path('board/', views.board, name = 'board'),
     path('create/', views.create, name = 'create'),
-    path('<int:pk>/', views.diary, name = 'diary'),
+    # /posts/5/ 
+    path('<int:pk>/', views.diarypage, name = 'diarypage'),
+    # /posts/5/update
+    path('<int:pk>/update', views.update, name='update'),
+    # /posts/5/delete
     path('<int:pk>/delete/', views.delete, name = 'delete'),
-    path('', views.main, name = 'main'),
 ]
