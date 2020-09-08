41cd3db6878a9cdbcf90b4ab90d62157fb558aa8
seunghyun2269 <akdntm2269@naver.com>
2020-09-09 02:46:38 +0900

    edit urls.py

--- diary/urls.py
+++ diary/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.main, name = 'main'),
     path('board/', views.board, name = 'board'),
     path('create/', views.create, name = 'create'),
-    # /posts/5/ 
+    # /diary/5/ 
     path('<int:post_id>/', views.diarypage, name = 'diarypage'),
-    # /posts/5/update
+    # /diary/5/update
     # path('<int:post_id>/update', views.update, name='update'),
-    # /posts/5/delete
+    # /diary/5/delete
     path('<int:post_id>/delete/', views.delete, name = 'delete'),
 ]
